3d3a8e141e981f49108e0c30e4e041248efec697
M. Faisal Rahman <faizal.btg@gmail.com>
2016-11-05 15:34:21 +0700

    create storyboard

---
+++ pokedex3/PokeCell.swift
@@ -0,0 +1,13 @@
+//
+//  PokeCell.swift
+//  pokedex3
+//
+//  Created by fzlrhmn on 11/5/16.
+//  Copyright © 2016 fzlrhmn. All rights reserved.
+//
+
+import UIKit
+
+class PokeCell: UICollectionViewCell {
+    
+}
