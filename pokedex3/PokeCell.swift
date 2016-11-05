5af1c43f1d76c55f6d83f4ecdbb1d016d645df21
M. Faisal Rahman <faizal.btg@gmail.com>
2016-11-05 15:44:45 +0700

    added viewcontroller for collection view cell

--- pokedex3/PokeCell.swift
+++ pokedex3/PokeCell.swift
@@ -10,4 +10,15 @@ import UIKit
 
 class PokeCell: UICollectionViewCell {
     
+    @IBOutlet weak var thumbImg: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    
+    var pokemon: Pokemon!
+    
+    func configureCell(pokemon: Pokemon) {
+        self.pokemon = pokemon
+        
+        nameLabel.text = self.pokemon.name.capitalized
+        thumbImg.image = UIImage(named: "\(self.pokemon.pokedexId)")
+    }
 }
